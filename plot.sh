75b4ca0f326447f88bfc0c5a375e0b2844ec74d0
Maggie Kuang <89272312+keyingkuang@users.noreply.github.com>
2025-03-17 14:47:06 -0700

    Update plot.sh

--- plot.sh
+++ plot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #SBATCH --mail-user=keying_kuang@berkeley.edu
 #SBATCH --mail-type=ALL
-python train_cifar10.py --net mlpmixer --n_epochs 300 --lr 1e-3
+python train_cifar10.py --net mlpmixer --n_epochs 300 --lr 1e-3 > mlp_train_log.txt 2>&1
