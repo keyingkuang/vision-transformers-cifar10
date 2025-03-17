54641d284079573ebf7b292116fb836ca7d4c1be
Maggie Kuang <89272312+keyingkuang@users.noreply.github.com>
2025-03-17 14:47:24 -0700

    Update plot2.sh

--- plot2.sh
+++ plot2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #SBATCH --mail-user=keying_kuang@berkeley.edu
 #SBATCH --mail-type=ALL
-python train_cifar10.py --net vit_small --n_epochs 300
+python train_cifar10.py --net vit_small --n_epochs 300 > vit_train_log.txt 2>&1
